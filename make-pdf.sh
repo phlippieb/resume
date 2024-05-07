925afa5e1442ea5a369ea055d7673a150340fa9a
phlippieb <phlippie.bosman@gmail.com>
2024-05-07 19:55:09 +0200

    Increase text size and fix how margins are set

--- make-pdf.sh
+++ make-pdf.sh
@@ -5,9 +5,9 @@ pandoc \
 --pdf-engine-opt=--enable-local-file-access \
 --metadata pagetitle="resume.md" \
 --css resume.css \
--V margin-top=0 \
--V margin-left=0 \
--V margin-right=0 \
--V margin-bottom=0 \
+-V margin-top=1in \
+-V margin-left=1in \
+-V margin-right=1in \
+-V margin-bottom=1in \
 resume.md \
 -o resume.pdf
