5bf4bf5d6cb48146773617006182823fd5b5e238
phlippieb <phlippie.bosman@gmail.com>
2024-05-07 19:49:09 +0200

    Set padding in CSS only and make 1inch

--- make-pdf.sh
+++ make-pdf.sh
@@ -5,5 +5,9 @@ pandoc \
 --pdf-engine-opt=--enable-local-file-access \
 --metadata pagetitle="resume.md" \
 --css resume.css \
+-V margin-top=0 \
+-V margin-left=0 \
+-V margin-right=0 \
+-V margin-bottom=0 \
 resume.md \
 -o resume.pdf
